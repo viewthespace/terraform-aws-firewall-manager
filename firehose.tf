d89fc6faf17787638fca1ef47464294c04dc2070
Cleydson <cleydson.andrade@vts.com>
2023-05-04 20:10:51 -0400

    Fix: Use newer CloudPosse S3 Module version to add encryption and policies

--- firehose.tf
+++ firehose.tf
@@ -12,7 +12,7 @@ module "firehose_label" {
 module "firehose_s3_bucket" {
   count                  = local.enabled && var.firehose_enabled ? 1 : 0
   source                 = "cloudposse/s3-bucket/aws"
-  version                = "0.44.1"
+  version                = "0.49.0"
   acl                    = "private"
   enabled                = true
   user_enabled           = true
